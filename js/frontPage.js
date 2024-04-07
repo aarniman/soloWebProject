ae9996253a776b84a2dbd606891df71672897d64
aarniman <122281928+aarniman@users.noreply.github.com>
2024-04-07 21:26:35 +0300

    First draft of the Lay-Out

---
+++ js/frontPage.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {scrollFunction()};
+
+const header = document.getElementById("myHeader");
+
+const sticky = header.offsetTop;
+
+function scrollFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
